5b2bc2611fc7285010c46b3ca6f75e0b10e7dc10
umashankar somasekar <umashankar somasekar>
2017-05-19 14:54:03 +0530

    mocidfication

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,42 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './app/javascripts/app.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js'
-  },
-  plugins: [
-    // Copy our app's index.html to the build folder.
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
-    ])
-  ],
-  module: {
-    rules: [
-      {
-       test: /\.css$/,
-       use: [ 'style-loader', 'css-loader' ]
-      }
+    entry: ['./app/javascripts/app.js',
+        './app/javascripts/bootstrap.min.js'
     ],
-    loaders: [
-      { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      }
-    ]
-  }
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.js'
+
+    },
+
+    plugins: [
+        // Copy our app's index.html to the build folder.
+        new CopyWebpackPlugin([
+            { from: './app/index.html', to: "index.html" }
+        ]),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }],
+        loaders: [
+            { test: /\.json$/, use: 'json-loader' },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-runtime']
+                }
+            }
+        ]
+    }
 }
